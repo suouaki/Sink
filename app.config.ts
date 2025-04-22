425abefed45911069053f5a3f3da3d6cbd1a92a4
suouaki <momobako@myyahoo.com>
2025-04-23 03:09:23 +0800

    Update app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -1,13 +1,13 @@
 export default defineAppConfig({
   title: 'Sink',
-  email: 'sink.cool@miantiao.me',
-  github: 'https://github.com/ccbikai/sink',
-  twitter: 'https://sink.cool/kai',
-  telegram: 'https://sink.cool/telegram',
-  mastodon: 'https://sink.cool/mastodon',
-  blog: 'https://sink.cool/blog',
+  email: 'aki@momobako.me',
+  github: 'https://github.com/suouaki',
+  twitter: 'https://x.com/ccMj2Urwo295132',
+  telegram: 'https://t.me/momo_bako',
+  mastodon: 'https://www.threads.net/@haisa_kura',
+  blog: 'http://momobako.atwebpages.com/',
   description: 'A Simple / Speedy / Secure Link Shortener with Analytics, 100% run on Cloudflare.',
-  image: 'https://sink.cool/banner.png',
+  image: 'https://mig01.996399.xyz/The%20Hungry%20Lamb%202024_12_15%201_09_15.png',
   previewTTL: 300, // 5 minutes
   slugRegex: /^[a-z0-9]+(?:-[a-z0-9]+)*$/i,
   reserveSlug: [
