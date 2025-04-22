a17cec1871dc098d3bcdb180d6b2f0052aec064f
suouaki <momobako@myyahoo.com>
2025-04-23 03:03:29 +0800

    Update Cta.vue

--- components/home/Cta.vue
+++ components/home/Cta.vue
@@ -10,7 +10,7 @@
     </p>
     <div class="flex mt-5">
       <HomeLink
-        href="https://github.com/ccbikai/sink?tab=readme-ov-file#%EF%B8%8F-deployment"
+        href="https://github.com/suouaki?tab=readme-ov-file#%EF%B8%8F-deployment"
         type="inverted"
         :title="$t('home.cta.button')"
       >
